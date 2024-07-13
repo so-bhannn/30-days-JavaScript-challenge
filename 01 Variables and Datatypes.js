a7813305c4d704a027361efb840fbb99fe31f6c3
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2024-07-13 20:11:40 +0530

    Day 1

---
+++ 01 Variables and Datatypes.js
@@ -0,0 +1,42 @@
+// Activity 1: Variable declaration
+
+// Task 1
+var num=10;
+console.log(num);
+
+//Task 2
+var string="chaicode";
+console.log(string);
+
+//Activity 2: Constant Declaration
+
+//Task 3:
+const bool=true;
+console.log(bool);
+
+//Activity 3: Data Types
+
+//Task 4:
+num1=15;
+str1="ChaiAurCode";
+bool1=false;
+obj1={name:"ChaiAurCode",type:"website"};
+arr1=Array(1,2,3,4,5);
+
+console.log(typeof num1);
+console.log(typeof str1);
+console.log(typeof bool1);
+console.log(typeof obj1);
+console.log(typeof arr1);
+
+//Activity 4: Reassigning Variables
+
+//Task 5:
+let variable="IWriteCode";
+console.log(variable)
+variable="WeWriteCode";
+console.log(variable)
+
+//Task 6:
+const constant="Shaktimaan";
+constant="Superman";
