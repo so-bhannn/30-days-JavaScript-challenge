c7fa6ebb9b6652a0e0ae91c50eb9bdaa75ef4186
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2024-07-14 21:54:02 +0530

    Day 2:Operators

---
+++ 02 Operators .js
@@ -0,0 +1,45 @@
+//Activity 1:Arithmetic Operators
+
+//Task1:
+console.log(10+5);
+//Task2:
+console.log(10-5);
+//Task3:
+console.log(10*5);
+//Task4:
+console.log(10/5);
+//Task5:
+console.log(10%6);
+
+//Activity2:Asssignment Operators
+var num=12;
+//Task6:
+console.log(num+=8);
+//Task7:
+console.log(num-=8);
+
+//Activity3:Comparison Operators
+
+//Task8:
+console.log(10>5);
+console.log(10<5);
+//Task9:
+console.log(10<=5);
+console.log(10>=5);
+//Task10:
+console.log(10==5);
+console.log(10===5);
+
+//Activity4:Logical Operators
+
+//Task11
+console.log('ChaiCode'=='chaicode'&&'shaktimaan'=='Gangadhar');
+//Task12:
+console.log('ChaiCode'=='chaicode'||'shaktimaan'=='Gangadhar');
+//Task13:
+console.log(!('ChaiCode'=='chaicode'||'shaktimaan'=='Gangadhar'));
+
+//Activity5:Ternary Operators
+
+//Task14:
+console.log(10>5 ? '10 is greater than 5' : '10 is less than 5');
